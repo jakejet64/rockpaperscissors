bbe422db65734f87b4c2494ee71fab8f7027f7cf
Jake Thomas <jakejet64@gmail.com>
2022-05-22 15:40:49 -0400

    Added textbox & hooked up all console logs to said textbox

--- javascript.js
+++ javascript.js
@@ -1,7 +1,13 @@
 let computerWins = 0;
 let playerWins = 0;
+let ties = 0;
 
 const buttons = document.querySelectorAll('button');
+const textBox = document.querySelector('.textBox');
+
+function totalRoundsPlayed() {
+    return (computerWins+playerWins+ties);
+}
 
 function computerMove(){
     let random = Math.random();
@@ -21,6 +27,7 @@ function getPlayerMove(){
 function playRound(playerMove, computerMove){
     if(playerMove == "rock"){
         if(computerMove == "rock"){
+            ties++;
             return("Tie! Rock doesn't hurt rock.");
         }else if(computerMove == "paper"){
             computerWins++;
@@ -34,6 +41,7 @@ function playRound(playerMove, computerMove){
             playerWins++;
             return("Win! Your paper covers the computers rock.");
         }else if(computerMove == "paper"){
+            ties++;
             return("Tie! Paper doesn't do anything to paper.");
         }else{ //computerMove == "scissors"
             computerWins++;
@@ -47,14 +55,34 @@ function playRound(playerMove, computerMove){
             playerWins++;
             return("Win! Your Scissors cut the computers paper.");
         }else{ //computerMove == "scissors"
+            ties++;
             return("Tie! Your scissors do nothing to the computers.")
         }
     }
 }
 
+function textBoxLog(text){
+    const temp = document.createElement('p');
+    temp.textContent = text;
+    temp.classList.add('textBoxText');
+    textBox.appendChild(temp);
+}
+
 function handleButton() {
-    console.log(playRound(this.className, computerMove()));
-    console.log("The verdict? Computer: " + computerWins + " Player:" + playerWins);
+    textBoxLog(playRound(this.className, computerMove()));
+    if(totalRoundsPlayed() >= 5){
+        buttons.forEach(button => button.removeEventListener('click', handleButton));
+        if(computerWins > playerWins){
+            textBoxLog(playerWins + ":" + computerWins + ", Computer wins!");
+        }else if(playerWins > computerWins){
+            textBoxLog(playerWins + ":" + computerWins + ", You Win!");
+        }else{
+            textBoxLog(playerWins + ":" + computerWins + ", tie!");
+        }
+
+    }
 }
 
 buttons.forEach(button => button.addEventListener('click', handleButton));
+
+textBoxLog('Best of five?');
